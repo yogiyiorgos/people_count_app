77266cf24368e88c94885e40f6a0a86cb38c3b21
yogiyiorgos <95660601+yogiyiorgos@users.noreply.github.com>
2022-01-09 22:54:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count    
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.tectContent = 0
+    count = 0
+}
